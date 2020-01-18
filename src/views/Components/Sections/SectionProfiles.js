60f255aa760e5a5a4033c841813b75486dd49afd
Joe Pagan <joepagan@gmail.com>
2020-01-18 10:52:39 +0000

    Forcing profile components to be lazyloaded

--- src/views/Components/Sections/SectionProfiles.js
+++ src/views/Components/Sections/SectionProfiles.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactPlayer from 'react-player';
 import { isMobile } from "react-device-detect";
+import LazyLoad from 'react-lazyload';
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 
@@ -37,130 +38,140 @@ export default function SectionProfile() {
           <GridContainer>
             <GridItem xs={6} md={4}>
               <h5 className={classes.name}>Tiph Milstar (Propz)</h5>
-              <img
-                src={tiph}
-                alt="Tiph Milstar"
-                className={
-                  classes.imgRaised +
-                  " " +
-                  classes.imgRounded +
-                  " " +
-                  classes.imgFluid
-                }
-              />
+              <LazyLoad once height={388}>
+                <img
+                  src={tiph}
+                  alt="Tiph Milstar"
+                  className={
+                    classes.imgRaised +
+                    " " +
+                    classes.imgRounded +
+                    " " +
+                    classes.imgFluid
+                  }
+                />
+              </LazyLoad>
               <h6 className={classes.role}>Lead Vocals</h6>
             </GridItem>
             <GridItem xs={6} md={4}>
               <h5 className={classes.name}>Mark Rinder</h5>
               <div className={classes.playerWrapper}>
-                <ReactPlayer
-                  className={
-                    classes.player +
-                    " " +
-                    classes.imgRaised +
-                    " " +
-                    classes.imgRounded +
-                    " " +
-                    classes.imgFluid
-                  }
-                  url={rinderVideo}
-                  config={{
-                    file: {
-                      attributes: {
-                        poster: rinderImage
-                      }
+                <LazyLoad once height={388}>
+                  <ReactPlayer
+                    className={
+                      classes.player +
+                      " " +
+                      classes.imgRaised +
+                      " " +
+                      classes.imgRounded +
+                      " " +
+                      classes.imgFluid
                     }
-                  }}
-                  light={isMobile ? rinderImage : false}
-                  playsinline
-                  autoPlay
-                  muted
-                  playing
-                  loop
-                  width="100%"
-                  height="100%"
-                />
+                    url={rinderVideo}
+                    config={{
+                      file: {
+                        attributes: {
+                          poster: rinderImage
+                        }
+                      }
+                    }}
+                    light={isMobile ? rinderImage : false}
+                    playsinline
+                    autoPlay
+                    muted
+                    playing
+                    loop
+                    width="100%"
+                    height="100%"
+                  />
+                </LazyLoad>
               </div>
               <h6 className={classes.role}>Guitar, Backing Vocals</h6>
             </GridItem>
             <GridItem xs={6} md={4}>
               <h5 className={classes.name}>Liam Sullivan</h5>
-              <img
-                src={liam}
-                alt="Liam Sullivan"
-                className={
-                  classes.imgRaised +
-                  " " +
-                  classes.imgRounded +
-                  " " +
-                  classes.imgFluid
-                }
-              />
-              <h6 className={classes.role}>Drums, Percussion</h6>
-            </GridItem>
-            <GridItem xs={6} md={4}>
-              <h5 className={classes.name}>Matt Hickey</h5>
-              <div className={classes.playerWrapper}>
-                <ReactPlayer
+              <LazyLoad once height={388}>
+                <img
+                  src={liam}
+                  alt="Liam Sullivan"
                   className={
-                    classes.player +
-                    " " +
                     classes.imgRaised +
                     " " +
                     classes.imgRounded +
                     " " +
                     classes.imgFluid
                   }
-                  url={hickeyVideo}
-                  config={{
-                    file: {
-                      attributes: {
-                        poster: hickeyImage
-                      }
-                    }
-                  }}
-                  light={isMobile ? hickeyImage : false}
-                  playsinline
-                  autoPlay
-                  muted
-                  playing
-                  loop
-                  width="100%"
-                  height="100%"
                 />
+              </LazyLoad>
+              <h6 className={classes.role}>Drums, Percussion</h6>
+            </GridItem>
+            <GridItem xs={6} md={4}>
+              <h5 className={classes.name}>Matt Hickey</h5>
+              <div className={classes.playerWrapper}>
+                <LazyLoad once height={388}>
+                  <ReactPlayer
+                    className={
+                      classes.player +
+                      " " +
+                      classes.imgRaised +
+                      " " +
+                      classes.imgRounded +
+                      " " +
+                      classes.imgFluid
+                    }
+                    url={hickeyVideo}
+                    config={{
+                      file: {
+                        attributes: {
+                          poster: hickeyImage
+                        }
+                      }
+                    }}
+                    light={isMobile ? hickeyImage : false}
+                    playsinline
+                    autoPlay
+                    muted
+                    playing
+                    loop
+                    width="100%"
+                    height="100%"
+                  />
+                </LazyLoad>
               </div>
               <h6 className={classes.role}>Synths/Piano</h6>
             </GridItem>
             <GridItem xs={6} md={4}>
               <h5 className={classes.name}>Joe Pagan</h5>
               <div className={classes.playerWrapper}>
-                <ReactPlayer
-                  className={
-                    classes.player +
-                    " " +
-                    classes.imgRaised +
-                    " " +
-                    classes.imgRounded +
-                    " " +
-                    classes.imgFluid
-                  }
-                  url={joeVideo}
-                  config={{
-                    file: {
-                      attributes: {
-                        poster: joeImage
-                      }
+                <LazyLoad once height={388}>
+                  <ReactPlayer
+                    className={
+                      classes.player +
+                      " " +
+                      classes.imgRaised +
+                      " " +
+                      classes.imgRounded +
+                      " " +
+                      classes.imgFluid
                     }
-                  }}
-                  light={isMobile ? joeImage : false}
-                  playsinline
-                  autoPlay
-                  muted
-                  playing
-                  loop
-                  width="100%"
-                  height="100%"
-                />
+                    url={joeVideo}
+                    config={{
+                      file: {
+                        attributes: {
+                          poster: joeImage
+                        }
+                      }
+                    }}
+                    light={isMobile ? joeImage : false}
+                    playsinline
+                    autoPlay
+                    muted
+                    playing
+                    loop
+                    width="100%"
+                    height="100%"
+                  />
+                </LazyLoad>
               </div>
               <h6 className={classes.role}>Bass, Backing Vocals</h6>
             </GridItem>
