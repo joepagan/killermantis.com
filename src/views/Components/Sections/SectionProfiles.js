d8e1b40f300b29e897c50283793b5c166ad6603c
Joe Pagan <joepagan@gmail.com>
2020-04-01 23:12:13 +0100

    Adding CJ profile tile Adding CJ video Adding CJ Image Updating full names to nicknames on profile tiles

--- src/views/Components/Sections/SectionProfiles.js
+++ src/views/Components/Sections/SectionProfiles.js
@@ -23,6 +23,8 @@ import hickeyImage from "assets/img/profiles/hickey-400.webp";
 import hickeyVideo from "assets/video/hickey.mp4";
 import joeImage from "assets/img/profiles/joe-400.webp";
 import joeVideo from "assets/video/joe.mp4";
+import cjImage from "assets/img/profiles/cj-400.webp";
+import cjVideo from "assets/video/cj.mp4";
 
 const useStyles = makeStyles(styles);
 
@@ -39,7 +41,7 @@ export default function SectionProfile() {
           <br />
           <GridContainer>
             <GridItem xs={6} md={4}>
-              <h5 className={classes.name}>Tiph Milstar (Propz)</h5>
+              <h5 className={classes.name}>Tiph (Propz)</h5>
               <div className={classes.playerWrapper}>
                 <LazyLoad once height={388}>
                   <ReactPlayer
@@ -71,10 +73,10 @@ export default function SectionProfile() {
                   />
                 </LazyLoad>
               </div>
-              <h6 className={classes.role}>Lead Vocals</h6>
+              <h6 className={classes.role}>Raps & Vocals</h6>
             </GridItem>
             <GridItem xs={6} md={4}>
-              <h5 className={classes.name}>Mark Rinder</h5>
+              <h5 className={classes.name}>Rinder</h5>
               <div className={classes.playerWrapper}>
                 <LazyLoad once height={388}>
                   <ReactPlayer
@@ -109,7 +111,7 @@ export default function SectionProfile() {
               <h6 className={classes.role}>Guitar, Backing Vocals</h6>
             </GridItem>
             <GridItem xs={6} md={4}>
-              <h5 className={classes.name}>Liam Sullivan</h5>
+              <h5 className={classes.name}>Sully</h5>
               <div className={classes.playerWrapper}>
                 <LazyLoad once height={388}>
                   <ReactPlayer
@@ -144,7 +146,7 @@ export default function SectionProfile() {
               <h6 className={classes.role}>Drums, Percussion</h6>
             </GridItem>
             <GridItem xs={6} md={4}>
-              <h5 className={classes.name}>Matt Hickey</h5>
+              <h5 className={classes.name}>Hick</h5>
               <div className={classes.playerWrapper}>
                 <LazyLoad once height={388}>
                   <ReactPlayer
@@ -179,7 +181,7 @@ export default function SectionProfile() {
               <h6 className={classes.role}>Synths/Piano</h6>
             </GridItem>
             <GridItem xs={6} md={4}>
-              <h5 className={classes.name}>Joe Pagan</h5>
+              <h5 className={classes.name}>Joe</h5>
               <div className={classes.playerWrapper}>
                 <LazyLoad once height={388}>
                   <ReactPlayer
@@ -212,6 +214,41 @@ export default function SectionProfile() {
                 </LazyLoad>
               </div>
               <h6 className={classes.role}>Bass, Backing Vocals</h6>
+            </GridItem>
+            <GridItem xs={6} md={4}>
+              <h5 className={classes.name}>CJ</h5>
+              <div className={classes.playerWrapper}>
+                <LazyLoad once height={388}>
+                  <ReactPlayer
+                    className={
+                      classes.player +
+                      " " +
+                      classes.imgRaised +
+                      " " +
+                      classes.imgRounded +
+                      " " +
+                      classes.imgFluid
+                    }
+                    url={cjVideo}
+                    config={{
+                      file: {
+                        attributes: {
+                          poster: cjImage
+                        }
+                      }
+                    }}
+                    light={isMobile ? cjImage : false}
+                    playsinline
+                    autoPlay
+                    muted
+                    playing
+                    loop
+                    width="100%"
+                    height="100%"
+                  />
+                </LazyLoad>
+              </div>
+              <h6 className={classes.role}>Lead Vocals</h6>
             </GridItem>
           </GridContainer>
           <GridContainer />
